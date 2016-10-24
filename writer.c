9b7ba54f5007961b0dc78bde28e28b9a58594ef4
Julián J. Méndez O <menoc.sk27@gmail.com>
2016-10-23 22:22:17 -0600

    Transcribir

--- writer.c
+++ writer.c
@@ -6,8 +6,19 @@
 #include <time.h>
 
 static FILE *results_file;
+static FILE *temp;
 
-void writeMessage (int pid) {
+void writeMessage (int pid, int line) {
+    
+    int line_to_write = line; //largo en chars + 1 (el \n)
+
+    results_file = fopen("results.txt", "r");
+    temp = fopen("temp.txt", "w+");
+
+    if (results_file == NULL) {
+        printf("ERROR: Can't open results file\n");
+    }
+    
     time_t rawtime;
     struct tm * timeinfo;
 
@@ -17,21 +28,38 @@ void writeMessage (int pid) {
 
     timestamp[0] = timeinfo->tm_mday; timestamp[1] = timeinfo->tm_mon + 1; timestamp[2] = timeinfo->tm_year + 1900; timestamp[3] = timeinfo->tm_hour; timestamp[4] = timeinfo->tm_min; timestamp[5] = timeinfo->tm_sec;
 
-    printf("\t PID: %i, Day: %i, Month: %i, Year: %i, Hour: %i, Min: %i, Sec: %i \n", pid, timestamp[0], timestamp[1], timestamp[2], timestamp[3], timestamp[4], timestamp[5]);    
-    fprintf(results_file,"\n\t PID: %i, Day: %i, Month: %i, Year: %i, Hour: %i, Min: %i, Sec: %i \n", pid, timestamp[0], timestamp[1], timestamp[2], timestamp[3], timestamp[4], timestamp[5]);
+    int count = 0;
+    char buf[256];
+
+    while (fgets(buf, sizeof(buf), results_file)){
+
+        if (count == line_to_write){
+            fputs("new new new line  \n", temp);    
+        }
+        
+        else {
+            fprintf(temp, "%s", buf);
+        }
+        count++;
+    }
+
+
+    //printf("\t PID: %i, Day: %i, Month: %i, Year: %i, Hour: %i, Min: %i, Sec: %i \n", pid, timestamp[0], timestamp[1], timestamp[2], timestamp[3], timestamp[4], timestamp[5]);    
+
+    //char str[] = "This is tutorialspoint.com";
+    //fwrite(str , 1 , sizeof(str) , results_file );
+    
+    //fprintf(results_file,"\n\t PID: %i, Day: %i, Month: %i, Year: %i, Hour: %i, Min: %i, Sec: %i \n", pid, timestamp[0], timestamp[1], timestamp[2], timestamp[3], timestamp[4], timestamp[5]);
+    fclose(results_file);
+    fclose(temp);
 }
+
 
 int main(int argc, char *argv[]){
 
 	int writers_amount; 
 	int write_time;
 	int sleep_time;
-
-    results_file = fopen("results.txt", "a");
-
-    if (results_file == NULL) {
-        printf("ERROR: Can't open results file\n");
-    }
 
     if( argc != 4 ) {
        	printf("\nERROR: 3 parameters expected: Amount_Of_Writers, Write_Time, Sleep_time to create. Program ended.\n\n");
@@ -54,8 +82,8 @@ int main(int argc, char *argv[]){
     }
 
     //Printea y escribe en archivo.
-    writeMessage(10);
+    writeMessage(10,3);
 
-    fclose(results_file);
+    
 
 }
